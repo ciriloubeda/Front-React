416702f9fec55857acff9d04d558f29d86f7ba4b
ciriloubeda <ciriloubeda@gmail.com>
2019-06-21 20:35:59 -0300

    version 2.0

--- src/App.js
+++ src/App.js
@@ -6,23 +6,24 @@ import Create from './components/create.component';
 import Edit from './components/edit.component';
 import Index from './components/index.component';
 
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to={'/'} className="navbar-brand">React Taller VI</Link>
+            <Link to={'/'} className="navbar-brand">React CRUD Example</Link>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                  <Link to={'/'} className="nav-link">Home</Link>
+                  <Link to={'/'} className="nav-link">Principal</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to={'/create'} className="nav-link">Create</Link>
+                  <Link to={'/create'} className="nav-link">Alta</Link>
                 </li>
                 <li className="nav-item">
-                  <Link to={'/index'} className="nav-link">Index</Link>
+                  <Link to={'/index'} className="nav-link">Listado</Link>
                 </li>
               </ul>
             </div>
